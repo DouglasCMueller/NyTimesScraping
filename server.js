ec56a91bb34d0ba67adabd8aeb05e06cd95d3b7d
Douglas Mueller <douglasmueller6@gmail.com>
2020-03-05 14:51:52 -0500

    cleaned up code

--- server.js
+++ server.js
@@ -157,10 +157,6 @@ app.get("/nytarticlesnotes/:id", function(req, res) {
         });
 });
 
-
-
-
-
 // Route for grabbing a specific Article by id and deleting
 app.get("/nytarticles/:id", function(req, res) {
     // console.log(res)
@@ -182,24 +178,6 @@ app.get("/nytarticles/:id", function(req, res) {
        
 });
 
-
-
-
-// // Route for grabbing a specific Article by id, populate it with it's note
-// app.get("/nytarticles/:id", function(req, res) {
-//     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//     db.NytArticle.findOne({ _id: req.params.id })
-//         // ..and populate all of the notes associated with it
-//         .populate("ArticleNote")
-//         .then(function(dbArticleNote) {
-//             // If we were able to successfully find an Article with the given id, send it back to the client
-//             res.json(dbArticleNote);
-//         })
-//         .catch(function(err) {
-//             // If an error occurred, send it to the client
-//             res.json(err);
-//         });
-// });
 app.get("/savednytarticles", function(req, res) {
         console.log(req, res)
         db.NytArticle.find({saved: true} )
@@ -208,25 +186,7 @@ app.get("/savednytarticles", function(req, res) {
             res.json(dbNytarticles)
         })
     })
-// // // Route for saving/updating an Article's associated Note
-// // app.post("/articles/:id", function(req, res) {
-// //     // Create a new note and pass the req.body to the entry
-// //     db.Note.create(req.body)
-// //         .then(function(dbNote) {
-// //             // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-// //             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-// //             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-// //             return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-// //         })
-// //         .then(function(dbArticle) {
-// //             // If we were able to successfully update an Article, send it back to the client
-// //             res.json(dbArticle);
-// //         })
-// //         .catch(function(err) {
-// //             // If an error occurred, send it to the client
-// //             res.json(err);
-// //         });
-// // });
+
 
 // Start the server
 app.listen(PORT, function() {
@@ -234,30 +194,3 @@ app.listen(PORT, function() {
 });
 
 
-
-
-
-// // Requiring the `Example` model for accessing the `examples` collection
-// var TimesExample = require("./models/exampleModel.js");
-
-// // Connect to the Mongo DB
-// mongoose.connect("mongodb://localhost/nytimesscrape", { useNewUrlParser: true });
-
-// // Create an object containing dummy data to save to the database
-// var data = {
-//     array: ["itemA", "itemB", "itemC"],
-//     boolean: false,
-//     string: "\"Don't worry if it doesn't work right. If everything did, you'd be out of a job\" - Mosher's Law of Software Engineering",
-//     number: 55
-// };
-
-// // Save a new Example using the data object
-// TimesExample.create(data)
-//     .then(function(dbExample) {
-//         // If saved successfully, print the new Example document to the console
-//         console.log(dbExample);
-//     })
-//     .catch(function(err) {
-//         // If an error occurs, log the error message
-//         console.log(err.message);
-//     });
